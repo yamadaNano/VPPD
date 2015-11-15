55453a6a4eae8be4443181300f3cbe6b3325c8cc
Daniel <deworrall92@gmail.com>
2015-11-15 13:40:27 +0000

    savename

---
+++ predictive.py
@@ -0,0 +1,13 @@
+'''Teacher predictive density'''
+
+import os, sys, time
+
+import numpy
+
+folder = './targets'
+data = numpy.zeros((50000,10))
+for i in range(100):
+    data += numpy.load(folder + '/t' + str(i) + '.npy')
+
+savename = './targets/tpred.npy'
+numpy.save(savename, data)
